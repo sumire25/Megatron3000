f72ea9c86786237c9ba51cd69ed2d3698bb66938
Yanira1612 <83191641+Yanira1612@users.noreply.github.com>
2024-06-27 19:06:37 -1000

    Update pageEdit.h comments

--- Query_Processor/pageEdit.h
+++ Query_Processor/pageEdit.h
@@ -23,8 +23,27 @@ namespace pageEdit {
      *@author Chullunquia Alonso
      */
     // HEADER PAGE
+    /**
+   * Obtiene y actualiza la página libre que tiene espacio suficiente para un registro.
+   * @param page: referencia a la cadena que representa la página.
+   * @param recordSize: tamaño del registro.
+   * @param isVar: booleano que indica si el registro es de longitud variable.
+   * @return ID de la página libre o -1 si no hay espacio suficiente.
+   */
     int getAndUpdateFreePage(std::string &page, int recordSize, bool isVar);
+
+ /**
+   * Establece un nuevo encabezado en la página.
+   * @param page: referencia a la cadena que representa la página.
+   */
     void setNewPageHeader(std::string &page);
+
+ /**
+    * Agrega una nueva página al directorio.
+    * @param page: referencia a la cadena que representa la página.
+    * @param PageID: ID de la nueva página.
+    * @param freeSpace: espacio libre en la nueva página.
+    */
     void addNewPageToDirectory(std::string &page, const int PageID, const int freeSpace);
     // UNPACKED PAGE
     int getTotalNumRecords( const std::string &page, int recordSize);
@@ -32,12 +51,10 @@ namespace pageEdit {
     void insertUnpacked(std::string &page, const std::string &record);
 
 
-   /**
-     *@author Suni Yanira
-     */
     //SLOTTED PAGE
     /**
     * Estructura que representa la cabecera de una página slotted.
+    * @author Suni Yanira
     */
     struct Cabecera;
 
@@ -46,6 +63,7 @@ namespace pageEdit {
      * @param outStream: flujo de salida para escribir.
      * @param value: valor a escribir.
      * @param size: tamaño del valor a escribir.
+     * @author Suni Yanira
      */
     void writeHeader(std::ostringstream& outStream, int value, size_t size);
 
@@ -53,6 +71,7 @@ namespace pageEdit {
   * Inicializa un bloque nuevo con formato slotted page.
   * @param blockSize: tamaño del bloque.
   * @return cadena que representa el bloque inicializado.
+  * @author Suni Yanira
   */
     std::string setNewSlotted(size_t blockSize);
 
@@ -60,6 +79,7 @@ namespace pageEdit {
     * Lee la cabecera de una página slotted.
     * @param data: cadena que representa la página.
     * @return estructura Cabecera que contiene los datos de la cabecera.
+    * @author Suni Yanira
     */
     Cabecera leerCabecera(const std::string& data);
 
@@ -68,9 +88,9 @@ namespace pageEdit {
  * @param data: cadena que representa la página.
  * @param registro: cadena que representa el nuevo registro.
  * @return cadena actualizada que representa la página con el nuevo registro.
+ * @author Suni Yanira
  */
     std::string insertSlotted(const std::string& data, const std::string& registro); //inserta un registro de longitud variable al bloque
 }
-
 
 #endif //PAGEEDIT_H
