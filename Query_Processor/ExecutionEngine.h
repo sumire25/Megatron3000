5ba4e41a542792e8d8596fab392e7fa9ca0995cb
Yanira1612 <83191641+Yanira1612@users.noreply.github.com>
2024-06-27 17:01:54 -1000

    Update ExecutionEngine.h comments

--- Query_Processor/ExecutionEngine.h
+++ Query_Processor/ExecutionEngine.h
@@ -13,25 +13,86 @@ class ExecutionEngine {
    *@author Suni Yanira
    */
 private:
+    /**
+ * Referencia al administrador de búfer.
+ */
   BufferManager* buffManRef;
+
+    /**
+ * Referencia al administrador de disco.
+ */
   DiskManager* diskManRef;
+
+    /**
+ * Tabla hash que almacena los esquemas de las relaciones.
+ * La clave es el nombre de la relación y el valor es un puntero al esquema correspondiente.
+ */
   unordered_map<string, Schema*> schemas;
 
-  //Insercion de registros de longitud variable en bloque (slotted page)
+    /**
+   * Inserta un registro de longitud variable en un bloque (slotted page).
+   * @param record: vector de strings que representa el registro.
+   */
   void insertVariableRecord(vector<string>& record);
-  //Insercion de registros de longitud fija en bloque (unpacked)
+
+    /**
+     * Inserta un registro de longitud fija en un bloque (unpacked).
+     * @param record: vector de strings que representa el registro.
+     */
   void insertFixedRecord(vector<string>& record);
-  //formatRecord devuelte un string ya con el formato indicado ya sea registro de longitud variable o registro de longitud fija
+
+
+    /**
+     * Devuelve un string con el formato indicado para un registro, ya sea de longitud variable o fija.
+     * @param record: vector de strings que representa el registro.
+     * @return string con el registro formateado.
+     */
   string formatRecord(vector<string>& record);
-  //Da el formato para registo de longitud fija
+
+    /**
+    * Da el formato para un registro de longitud fija.
+    * @param record: vector de strings que representa el registro.
+    * @param schema: esquema de la relación.
+    * @return string con el registro formateado.
+    */
   string fixedRecord(vector<string>& record, Schema* schema);
-  //Da el formato para registo de longitud variable
+
+    /**
+ * Da el formato para un registro de longitud variable.
+ * @param record: vector de strings que representa el registro.
+ * @param schema: esquema de la relación.
+ * @return string con el registro formateado.
+ */
   string variableRecord(vector<string> record,const Schema& schema);
-  //helpers
-  int getBlock(string& relName);//return header page del file de la relacion
-  bool hasVarRecords(string& relName); //verifica si existe regustros de longitud variable en la relacion
-  string schemaToString(Schema* schema); //del objeto schema convierte a string
-  vector<string> stringToVector(string &schemaStr, int &pageId); //de un string convierte en un schema
+
+    /**
+ * Obtiene el bloque de encabezado de la relación.
+ * @param relName: nombre de la relación.
+ * @return ID del bloque de encabezado.
+ */
+  int getBlock(string& relName);
+
+    /**
+ * Verifica si existen registros de longitud variable en la relación.
+ * @param relName: nombre de la relación.
+ * @return true si la relación tiene registros de longitud variable, false en caso contrario.
+ */
+  bool hasVarRecords(string& relName);
+
+    /**
+  * Convierte un esquema a una cadena.
+  * @param schema: puntero al esquema.
+  * @return string que representa el esquema.
+  */
+  string schemaToString(Schema* schema);
+
+  /**
+ * Convierte una cadena en un esquema.
+ * @param schemaStr: cadena que representa el esquema.
+ * @param pageId: referencia al ID de la página.
+ * @return vector de strings que representa el esquema.
+ */
+  vector<string> stringToVector(string &schemaStr, int &pageId);
 public:
   /**
    *@author Chullunquia Alonso
